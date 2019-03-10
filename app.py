8f767e0efc7936819958db07b8ffc84934fcfc6e
DC <just-one@hotmail.co.jp>
2019-03-10 14:36:46 +0900

    ユーザからの入力の合計値を出せるようにした

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
+from calculation import calculation_total
+
 if __name__ == '__main__':
     # ユーザからの入力を受け取る
     input_data = input("データを入力してください(スペース区切り) > ")
 
-    print(input_data)
+    # print(input_data)
 
     # 文字列のリストに変換する
     numbers_as_str = input_data.split(" ")
-    print(numbers_as_str)
+    # print(numbers_as_str)
 
     # 整数リストに変換する
     numbers = []  # からのリストを作る(空っぽの箱を作ってそこに入れていくイメージ)
 
+    # input された情報を整数に
     for number_as_str in numbers_as_str:
         int_num = int(number_as_str)  # 整数に変換
 
         numbers.append(int_num)  # numbersというリストに要素を追加
 
-    print(numbers)
+    # print(numbers)
 
-    # 　input された情報を整数に
 
     # 各統計量を計算する(合計、平均、・・)
+    total = calculation_total(numbers)
+
+    # print(total)
 
     # ユーザに見やすいようにフォーマットする
 
     # 出力する
-
-    # 必要な関数はなにか
+    print(f"合計; {total}")
